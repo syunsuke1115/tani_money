cb11e688e6d956280424b98db0899ced2d62acd4
syunsuke1115 <kinopio1115@gmail.com>
2022-01-07 15:43:31 +0900

    fixed homescreen UI

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tanimy/models/botton_common.dart';
 
 import 'LoginScreen.dart';
 import 'SignUpScreen.dart';
@@ -15,76 +16,93 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-        child: Center(
+      child: Center(
         child: Column(
-        children: [
-        SizedBox(
-        height: 50,
-    ),
-    Text(
-    "Tani money",
-    style: TextStyle(
-    fontSize: 45,
-    color: Colors.blue,
-    fontWeight: FontWeight.bold,
-    ),
-    ),
-    SizedBox(
-    height: 30,
-    ),
-    Text(
-    "あなたの覚悟に値段はつけられますか？",
-    style: TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-    ),
-    ),
-    SizedBox(
-    height: 18,
-    ),
-    Text(
-    "これでもう単位を落とすことはありません",
-    style: TextStyle(
-    fontSize: 15,
-    ),
-    ),
-    SizedBox(
-    height: 10,
-    ),
-    Text(
-    "充実した大学生活を送りましょう",
-    style: TextStyle(
-    fontSize: 15,
-    ),
-    ),
-    SizedBox(
-    height: 10,
-    ),
-    ElevatedButton(
-    onPressed: () => startLogin(context), child: Text("ログイン")),
-    SizedBox(
-    height: 10,
-    ),
-    ElevatedButton(
-    onPressed: () => startSignUp(context), child: Text("新規登録")),
-    SizedBox(
-    height: 15,
-    ),
-    Text(
-    "※このアプリは自分が落とさないと決意した単位に対して課金するアプリです",
-    style: TextStyle(fontSize: 9),
-    )
-    ],
-    ),
-    ),
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Tani money",
+              style: TextStyle(
+                fontSize: 45,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "あなたの覚悟に値段はつけられますか？",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              "単位を落としたら課金されるアプリです",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "これでもう単位を落とすことはありません",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "充実した大学生活を送りましょう",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Container(
+              height: 300,
+                width: 300,
+                child: Image.asset("images/title.png")),
+
+            ButtonCommon(
+                onPressed: () =>
+                    startLogin(context),
+                label: "ログイン",
+                color: Colors.blue),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              child: const Text('新規登録',style: TextStyle(fontSize: 16.0,fontFamily: "Mont"),),
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+              ),
+              onPressed: () => startSignUp(context),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      ),
     ));
   }
+
   startLogin(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 
   startSignUp(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SignUpScreen()));
   }
-
 }
