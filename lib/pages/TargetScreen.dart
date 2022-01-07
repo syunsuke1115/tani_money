4b9602ad883832ae0ddc9d69f42c7d219adc4b98
syunsuke1115 <kinopio1115@gmail.com>
2022-01-07 14:43:02 +0900

    targetscreenをfirebaseと接続

--- lib/pages/TargetScreen.dart
+++ lib/pages/TargetScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'TargetAddPage.dart';
 
 class TargetScreen extends StatefulWidget {
@@ -8,46 +9,73 @@ class TargetScreen extends StatefulWidget {
 }
 
 class _TargetScreenState extends State<TargetScreen> {
-  // Todoリストのデータ
-  List<String> targetList = [];
-
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    final uid = user!.uid;
+    final Stream<QuerySnapshot> _targetsStream = FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .collection("targets")
+        .snapshots();
+    List<String> targetList = [];
+
     return Scaffold(
       // AppBarを表示し、タイトルも設定
       appBar: AppBar(
         title: Text('リスト一覧'),
       ),
       // データを元にListViewを作成
-      body: ListView.builder(
-        itemCount: targetList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(targetList[index]),
+      body: Container(
+        child: Column(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+              stream: _targetsStream,
+              builder:
+                  (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("Loading");
+                }
+                return Expanded(
+                  child: ListView(
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data() as Map<String, dynamic>;
+                      return Card(
+                        child: ListTile(
+                          title: Text(data["subjectName"]),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // "push"で新規画面に遷移
-          // リスト追加画面から渡される値を受け取る
-          final newListText = await Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              // 遷移先の画面としてリスト追加画面を指定
-              return TargetAddPage();
-            }),
-          );
-          if (newListText != null) {
-            // キャンセルした場合は newListText が null となるので注意
-            setState(() {
-              // リスト追加
-              targetList.add(newListText);
-            });
-          }
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: Container(
+        child: FloatingActionButton(
+          onPressed: () async {
+            // "push"で新規画面に遷移
+            // リスト追加画面から渡される値を受け取る
+            final newListText = await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                // 遷移先の画面としてリスト追加画面を指定
+                return TargetAddPage();
+              }),
+            );
+            if (newListText != null) {
+              // キャンセルした場合は newListText が null となるので注意
+              setState(() {
+                // リスト追加
+                targetList.add(newListText);
+              });
+            }
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
