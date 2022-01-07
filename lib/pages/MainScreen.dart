8d3a9f6509c76d7477c77344983d276191f09fea
yutoffff <yutifukui3423@gmail.com>
2022-01-07 16:04:27 +0900

    画面遷移作成完了

--- lib/pages/MainScreen.dart
+++ lib/pages/MainScreen.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:intl/intl.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'MyPage.dart';
-import 'SubmitScreen.dart';
-import 'TargetScreen.dart';
-import 'TargetAddPage.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -16,22 +9,11 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  var _selectIndex = 0;
-  var _pages = <Widget>[];
-  //var _label = '';
-  var _titles = ['メイン', '目標設定', '成績提出', '設定'];
   String name = "hogehoge";
   static DateTime setDate = DateTime(2021, 12, 24, 14, 28);
   static DateFormat outputFormat = DateFormat('yyyy年MM月dd日');
   String dateString = outputFormat.format(setDate);
   List<String> lessonList = ["プログラミング基礎", "安全学基礎", "システム創成学基礎"];
-
-  void _onPageChanged(int index) {
-    setState(() {
-      _selectIndex = index;
-    });
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -59,62 +41,6 @@ class _MainScreenState extends State<MainScreen> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            label: _titles[0],
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: _titles[1],
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: _titles[2],
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: _titles[3],
-            backgroundColor: Colors.blue,
-          ),
-        ],
-        currentIndex: _selectIndex,
-        //onTap: _onTapItem,
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.of(context).pushNamed("/home");
-              break;
-            case 1:
-              Navigator.of(context).pushNamed("/target");
-              break;
-            case 2:
-              Navigator.of(context).pushNamed("/submit");
-              break;
-            case 3:
-              Navigator.of(context).pushNamed("/setting");
-              break;
-          }
-        },
-      ),
     );
-  }
-
-  startTargetScreen(BuildContext context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => TargetScreen()));
-  }
-
-  startSubmitScreen(BuildContext context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => SubmitScreen()));
-  }
-
-  startMyPage(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => MyPage()));
   }
 }
