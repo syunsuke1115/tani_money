67fea9d9bce98d6f2715fdfe09788137d429acd9
syunsuke1115 <kinopio1115@gmail.com>
2022-01-07 15:27:23 +0900

    added dropdown

--- lib/pages/TargetScreen.dart
+++ lib/pages/TargetScreen.dart
@@ -9,6 +9,41 @@ class TargetScreen extends StatefulWidget {
 }
 
 class _TargetScreenState extends State<TargetScreen> {
+  List<DropdownMenuItem<int>> _items = [];
+  int _selectItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    setItems();
+    _selectItem = _items[0].value!;
+  }
+
+  void setItems() {
+    _items
+      ..add(DropdownMenuItem(
+        child: Text(
+          '得単',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        value: 1,
+      ))
+      ..add(DropdownMenuItem(
+        child: Text(
+          '優',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        value: 2,
+      ))
+      ..add(DropdownMenuItem(
+        child: Text(
+          '優上',
+          style: TextStyle(fontSize: 20.0),
+        ),
+        value: 3,
+      ));
+  }
+
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth auth = FirebaseAuth.instance;
@@ -32,19 +67,29 @@ class _TargetScreenState extends State<TargetScreen> {
           children: [
             StreamBuilder<QuerySnapshot>(
               stream: _targetsStream,
-              builder:
-                  (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Text("Loading");
                 }
                 return Expanded(
                   child: ListView(
-                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
                       Map<String, dynamic> data =
                           document.data() as Map<String, dynamic>;
                       return Card(
                         child: ListTile(
                           title: Text(data["subjectName"]),
+                          trailing: DropdownButton(
+                            items: _items,
+                            value: _selectItem,
+                            onChanged: (int? value) => {
+                              setState(() {
+                                _selectItem = value!;
+                              }),
+                            },
+                          ),
                         ),
                       );
                     }).toList(),
