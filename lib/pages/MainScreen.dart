1c5a233247afdd217078b33062ed62d164e50104
syunsuke1115 <kinopio1115@gmail.com>
2022-01-14 23:19:53 +0900

    課金額を設定

--- lib/pages/MainScreen.dart
+++ lib/pages/MainScreen.dart
@@ -78,59 +78,64 @@ class _MainScreenState extends State<MainScreen> {
       body: Container(
         child: FutureBuilder<DocumentSnapshot>(
             future: users.doc(uid).get(),
-          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading");
-            }
-            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-            return Column(
-              children: [
-                Center(
-                  child: Text(
-                    homeMessage(data),
-                    style: TextStyle(
-                      fontSize: 18,
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+              return Column(
+                children: [
+                  SizedBox(height: 10),
+                  Center(
+                    child: Text(
+                      homeMessage(data),
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                StreamBuilder<QuerySnapshot>(
-                  stream: _targetsStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
+                  MoneyMessage(data),
+                  SizedBox(height: 20),
+                  StreamBuilder<QuerySnapshot>(
+                    stream: _targetsStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return Expanded(
+                        child: ListView(
+                          children: snapshot.data!.docs
+                              .map((DocumentSnapshot document) {
+                            Map<String, dynamic> data =
+                                document.data() as Map<String, dynamic>;
+                            return Card(
+                                child: ListTile(
+                              title: Text(data["subjectName"]),
+                              trailing: DropdownButton(
+                                items: _items,
+                                value: _selectItem,
+                                onChanged: (int? value) => {
+                                  setState(() {
+                                    _selectItem = value!;
+                                  }),
+                                },
+                              ),
+                            ));
+                          }).toList(),
+                        ),
                       );
-                    }
-                    return Expanded(
-                      child: ListView(
-                        children:
-                            snapshot.data!.docs.map((DocumentSnapshot document) {
-                          Map<String, dynamic> data =
-                              document.data() as Map<String, dynamic>;
-                          return Card(
-                              child: ListTile(
-                            title: Text(data["subjectName"]),
-                            trailing: DropdownButton(
-                              items: _items,
-                              value: _selectItem,
-                              onChanged: (int? value) => {
-                                setState(() {
-                                  _selectItem = value!;
-                                }),
-                              },
-                            ),
-                          ));
-                        }).toList(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            );
-          }
-        ),
+                    },
+                  ),
+                ],
+              );
+            }),
       ),
       floatingActionButton: Container(
         margin: EdgeInsets.only(bottom: 50.0),
@@ -160,14 +165,44 @@ class _MainScreenState extends State<MainScreen> {
 
   String homeMessage(Map<String, dynamic> data) {
     String message = "";
-    print(data);
-    if(data["targetFrag"] == true){
-      message = "${data["nickname"]}さんは以下の目標を設定しました。\n諦めずにがんばりましょう。";
-    }else{
+    if (data["targetFrag"] == true) {
+      message = "${data["nickname"]}さんは以下の目標を設定しました\n           諦めずにがんばりましょう";
+    } else {
       message = "右下のプラスボタンから目標を追加してください";
     }
     return message;
   }
+
+  Widget MoneyMessage(Map<String, dynamic> data) {
+    if (data["targetFrag"] == true) {
+      int fine = 100;
+      if (data["fine"] != null) {
+        fine = data["fine"];
+      }
+      return Column(children: [
+        SizedBox(height: 10),
+        Center(
+          child: RichText(text: TextSpan(
+            children: [
+              TextSpan(text: "1単位落とすと",style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),),
+              TextSpan(text: fine.toString(), style: TextStyle(
+                fontSize: 24,
+                  color:Colors.red,
+                  fontWeight: FontWeight.bold,
+              ),),
+              TextSpan(text: "円課金されます",style: TextStyle(
+                fontSize: 18,
+                  color: Colors.black
+              ),),
+            ],),
+          ),
+        ),
+      ]);
+    } else {
+      return SizedBox(height: 1);
+    }
+  }
 }
-
-
